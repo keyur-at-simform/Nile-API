edaa0c8c1fa450c0fd43bf72eecd213c9f49332b
Keyur Patel <keyur.patel@simformsolutions.com>
2023-01-08 16:16:01 +0530

    associated book with author

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_06_091355) do
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_103447) do
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "author_id", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
